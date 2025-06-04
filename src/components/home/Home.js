b7cb694db5831d89806c7dc9c17ae38d0acab035
randrianjarisoa <randrianjarisoa@gmail.com>
2025-06-04 23:17:07 +0300

    feat(sidebar): add icon sidebar

---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import "./Home.css";
+const Home = () => {
+  return (
+    <div className="container-fluid home">
+      <div className="container home-content">
+        <h1>Hi I'Am</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
